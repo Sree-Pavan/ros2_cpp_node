b766d86453f0642bf0f27a38ab3ca79c3eb04d8d
Sree Pavan Goli <pavanor21@gmail.com>
2022-08-12 16:43:48 +0000

    first commit

---
+++ my_custom_pkg/include/my_custom_pkg/cpp_header.hpp
@@ -0,0 +1,13 @@
+#include "rclcpp/rclcpp.hpp"
+
+
+class MyCustomNode : public rclcpp::Node
+{
+    public:
+        MyCustomNode() : Node("my_node")
+        {
+            RCLCPP_INFO(this->get_logger(), "Test cpp node");
+        }
+        
+    private:
+};
